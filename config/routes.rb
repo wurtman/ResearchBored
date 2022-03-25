3152d87f186c8614602a0e2b654a286940818d67
Stuart Wurtman <wurtman@protonmail.com>
2022-03-25 15:48:57 +0000

    Added Projects and Status tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :projects
+  resources :contents
+  get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
 end
